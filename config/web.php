9fd71236860a44b445e9179f3f3a0579faf3f018
Thiago Senna <trsenna@gmail.com>
2015-05-14 21:59:45 -0300

    saving progress

--- config/web.php
+++ config/web.php
@@ -1,22 +1,41 @@
 <?php
 
-$params = require(__DIR__ . '/params.php');
-
 $config = [
     'id' => 'basic',
-    'basePath' => dirname(__DIR__),
-    'bootstrap' => ['log'],
+    'basePath' => dirname( __DIR__ ),
+    'bootstrap' => [ 'log' ],
+
+    /*
+     * components
+     */
+
     'components' => [
+
+        /*
+         * components/request
+         */
+
         'request' => [
-            // !!! insert a secret key in the following (if it is empty) - this is required by cookie validation
-            'cookieValidationKey' => '',
+
+            /*
+             * Insert a secret key in the following
+             * This is required by cookie validation
+             */
+
+            'cookieValidationKey' => 'W8p?(G4k32p@S5M',
         ],
+
+        /*
+         * components/cache
+         */
+
         'cache' => [
             'class' => 'yii\caching\FileCache',
         ],
+
         'user' => [
             'identityClass' => 'app\models\User',
-            'enableAutoLogin' => true,
+            'enableAutoLogin' => TRUE,
         ],
         'errorHandler' => [
             'errorAction' => 'site/error',
@@ -26,29 +45,29 @@ $config = [
             // send all mails to a file by default. You have to set
             // 'useFileTransport' to false and configure a transport
             // for the mailer to send real emails.
-            'useFileTransport' => true,
+            'useFileTransport' => TRUE,
         ],
         'log' => [
             'traceLevel' => YII_DEBUG ? 3 : 0,
             'targets' => [
                 [
                     'class' => 'yii\log\FileTarget',
-                    'levels' => ['error', 'warning'],
+                    'levels' => [ 'error', 'warning' ],
                 ],
             ],
         ],
-        'db' => require(__DIR__ . '/db.php'),
+        'db' => require( __DIR__ . '/db.php' ),
     ],
-    'params' => $params,
+    'params' => require( __DIR__ . '/params.php' ),
 ];
 
-if (YII_ENV_DEV) {
+if ( YII_ENV_DEV ) {
     // configuration adjustments for 'dev' environment
-    $config['bootstrap'][] = 'debug';
-    $config['modules']['debug'] = 'yii\debug\Module';
+    $config[ 'bootstrap' ][ ] = 'debug';
+    $config[ 'modules' ][ 'debug' ] = 'yii\debug\Module';
 
-    $config['bootstrap'][] = 'gii';
-    $config['modules']['gii'] = 'yii\gii\Module';
+    $config[ 'bootstrap' ][ ] = 'gii';
+    $config[ 'modules' ][ 'gii' ] = 'yii\gii\Module';
 }
 
 return $config;
--- web/index.php
+++ web/index.php
@@ -1,12 +1,15 @@
 <?php
 
-// comment out the following two lines when deployed to production
-defined('YII_DEBUG') or define('YII_DEBUG', true);
-defined('YII_ENV') or define('YII_ENV', 'dev');
+/*
+ * comment out the following two lines when deployed to production
+ */
 
-require(__DIR__ . '/../vendor/autoload.php');
-require(__DIR__ . '/../vendor/yiisoft/yii2/Yii.php');
+defined( 'YII_DEBUG' ) or define( 'YII_DEBUG', TRUE );
+defined( 'YII_ENV' ) or define( 'YII_ENV', 'dev' );
 
-$config = require(__DIR__ . '/../config/web.php');
+require( __DIR__ . '/../vendor/autoload.php' );
+require( __DIR__ . '/../vendor/yiisoft/yii2/Yii.php' );
 
-(new yii\web\Application($config))->run();
+$config = require( __DIR__ . '/../config/web.php' );
+
+( new yii\web\Application( $config ) )->run();
